8626b8ba4fae37ceebc5fed92df183122703c4fb
Jerry Wu <iamjerrywu@gmail.com>
2021-06-21 22:37:24 +0800

    check in jmeter test finished

---
+++ Jiuzhang_Spring_E-Commerce_Spike_system/Code/2_Env_Page_setup/seckill_mybatis_reverse_engineering/src/main/java/com/example/seckill_mybatis_reverse_engineering/web/SeckillOverSellController.java
@@ -0,0 +1,24 @@
+package com.example.seckill_mybatis_reverse_engineering.web;
+
+import com.example.seckill_mybatis_reverse_engineering.services.SeckillOverSellService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SeckillOverSellController {
+    @Autowired
+    private SeckillOverSellService seckillOverSellService;
+    /**
+     * 简单 处理抢购请求
+     * @param seckillActivityId
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("/seckill/{seckillActivityId}")
+    public String seckil(@PathVariable long seckillActivityId){
+        return seckillOverSellService.processSeckill(seckillActivityId);
+    }
+}
